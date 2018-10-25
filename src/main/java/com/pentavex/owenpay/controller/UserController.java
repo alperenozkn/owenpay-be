c2de89130e6be7b27c5442bfcc3c5ce8b36841bf
birkandenizer <denizerb@itu.edu.tr>
2018-10-25 21:23:02 +0300

    Initial auth test

--- src/main/java/com/pentavex/owenpay/controller/UserController.java
+++ src/main/java/com/pentavex/owenpay/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import swaggergen.controller.UserApi;
 import swaggergen.model.CreateUserRequest;
 import swaggergen.model.CreateUserResponse;
+import swaggergen.model.GetUserResponse;
 
 import javax.validation.Valid;
 
@@ -17,6 +18,14 @@ public class UserController implements UserApi {
     public ResponseEntity<CreateUserResponse> createUser(@Valid @RequestBody CreateUserRequest request) {
         CreateUserResponse response = new CreateUserResponse();
         response.setMessage(request.getUsername());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Override
+    public ResponseEntity<GetUserResponse> getUsers(){
+        GetUserResponse response = new GetUserResponse();
+        response.setUsernames("{\"firstname\":\"Richard\", \"lastname\":\"Feynman\"}," +
+                "{\"firstname\":\"Marie\",\"lastname\":\"Curie\"}");
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
