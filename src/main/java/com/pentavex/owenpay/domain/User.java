d2f8e0a84b159ff8989d107013f16df67fde13c8
Bgiris <berkaygiris@gmail.com>
2018-10-29 20:48:46 +0300

    feat: adds unique column attribute to username

--- src/main/java/com/pentavex/owenpay/domain/User.java
+++ src/main/java/com/pentavex/owenpay/domain/User.java
@@ -1,5 +1,6 @@
 package com.pentavex.owenpay.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,7 +14,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String username;
+
     private String password;
 
     public User() { }
